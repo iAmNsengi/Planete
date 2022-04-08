4f440b98c0e0b3e4228f80c9782ea61d4a7681da
Eliezer Nsengi <iamnsengi@icloud.com>
2022-04-08 19:01:18 +0200

    fx: loading positioning

--- client/src/components/Gallery/Gallery.tsx
+++ client/src/components/Gallery/Gallery.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Highlight } from "../ui/hero-highlight";
 import Pagination from "./Pagination";
+import { FaTruckLoading } from "react-icons/fa";
 
 interface Image {
   src: string;
@@ -97,127 +98,131 @@ const Gallery: React.FC = () => {
 
   return (
     <>
-      {loading ? (
-        <p className="text-center text-2xl">Loading images...</p>
-      ) : (
-        <main className="bg-neutral-200" id="gallery">
-          <section className="px-4 py-24 mx-auto max-w-7xl">
-            <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-              <h2 className="text-center text-black text-5xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-                Gall
-                <Highlight className="bg-cyan-800 text-white">ery</Highlight>
-                <br />
-              </h2>
-            </div>
-          </section>
-          <Pagination
-            totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-          />
+      <main className="bg-neutral-200" id="gallery">
+        <section className="px-4 py-24 mx-auto max-w-7xl">
+          <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
+            <h2 className="text-center text-black text-5xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+              Gall
+              <Highlight className="bg-cyan-800 text-white">ery</Highlight>
+              <br />
+            </h2>
+          </div>
+        </section>
+        {loading ? (
+          <p className="text-center text-2xl border py-40 ">
+            <FaTruckLoading /> Loading images....
+          </p>
+        ) : (
+          <>
+            <Pagination
+              totalPages={totalPages}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
 
-          <section>
-            <div className="w-full h-full min-h-screen select-none">
-              <div
-                className="max-w-6xl mx-auto duration-1000 delay-300 opacity-0 select-none ease animate-fade-in-view px-2 lg:px-0"
-                style={{ opacity: 1, transform: "translate(0px, 0px)" }}
-              >
-                <ul
-                  id="gallery"
-                  className="grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-3"
+            <section>
+              <div className="w-full h-full min-h-screen select-none">
+                <div
+                  className="max-w-6xl mx-auto duration-1000 delay-300 opacity-0 select-none ease animate-fade-in-view px-2 lg:px-0"
+                  style={{ opacity: 1, transform: "translate(0px, 0px)" }}
                 >
-                  {paginatedImages.map((image, index) => (
-                    <li
-                      key={index}
-                      className="relative group cursor-zoom-in"
-                      onClick={() =>
-                        openGallery(currentPage * imagesPerPage + index)
-                      }
-                    >
-                      <img
+                  <ul
+                    id="gallery"
+                    className="grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-3"
+                  >
+                    {paginatedImages.map((image, index) => (
+                      <li
+                        key={index}
+                        className="relative group cursor-zoom-in"
                         onClick={() =>
                           openGallery(currentPage * imagesPerPage + index)
                         }
-                        src={image.src}
-                        className="object-cover select-none w-full h-auto bg-gray-200 rounded-xl cursor-zoom-in aspect-[5/6] lg:aspect-[1/3] xl:aspect-[3/4]"
-                        alt={image.alt}
-                      />
-                      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl">
-                        <p className="text-white text-center px-4">
-                          {image.description}
-                        </p>
+                      >
+                        <img
+                          onClick={() =>
+                            openGallery(currentPage * imagesPerPage + index)
+                          }
+                          src={image.src}
+                          className="object-cover select-none w-full h-auto bg-gray-200 rounded-xl cursor-zoom-in aspect-[5/6] lg:aspect-[1/3] xl:aspect-[3/4]"
+                          alt={image.alt}
+                        />
+                        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl">
+                          <p className="text-white text-center px-4">
+                            {image.description}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {galleryOpened && (
+                  <div
+                    onClick={closeGallery}
+                    className="fixed inset-0 z-[99] flex items-center justify-center bg-black bg-opacity-50 select-none cursor-zoom-out"
+                  >
+                    <div className="relative flex items-center justify-center w-11/12 xl:w-4/5 h-11/12">
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          prevImage();
+                        }}
+                        className="absolute left-0 flex items-center justify-center text-white translate-x-10 rounded-full cursor-pointer xl:-translate-x-24 2xl:-translate-x-32 bg-white/10 w-14 h-14 hover:bg-white/20"
+                      >
+                        <svg
+                          className="w-6 h-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15.75 19.5L8.25 12l7.5-7.5"
+                          />
+                        </svg>
                       </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              {galleryOpened && (
-                <div
-                  onClick={closeGallery}
-                  className="fixed inset-0 z-[99] flex items-center justify-center bg-black bg-opacity-50 select-none cursor-zoom-out"
-                >
-                  <div className="relative flex items-center justify-center w-11/12 xl:w-4/5 h-11/12">
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        prevImage();
-                      }}
-                      className="absolute left-0 flex items-center justify-center text-white translate-x-10 rounded-full cursor-pointer xl:-translate-x-24 2xl:-translate-x-32 bg-white/10 w-14 h-14 hover:bg-white/20"
-                    >
-                      <svg
-                        className="w-6 h-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
+                      <img
+                        className="object-cover w-fit h-fit select-none cursor-zoom-out"
+                        src={activeImageUrl || undefined}
+                        alt=""
+                      />
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          nextImage();
+                        }}
+                        className="absolute right-0 flex items-center justify-center text-white -translate-x-10 rounded-full cursor-pointer xl:translate-x-24 2xl:translate-x-32 bg-white/10 w-14 h-14 hover:bg-white/20"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15.75 19.5L8.25 12l7.5-7.5"
-                        />
-                      </svg>
-                    </div>
-                    <img
-                      className="object-cover w-fit h-fit select-none cursor-zoom-out"
-                      src={activeImageUrl || undefined}
-                      alt=""
-                    />
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        nextImage();
-                      }}
-                      className="absolute right-0 flex items-center justify-center text-white -translate-x-10 rounded-full cursor-pointer xl:translate-x-24 2xl:translate-x-32 bg-white/10 w-14 h-14 hover:bg-white/20"
-                    >
-                      <svg
-                        className="w-6 h-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                        />
-                      </svg>
+                        <svg
+                          className="w-6 h-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                          />
+                        </svg>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
-            </div>
-          </section>
-          <Pagination
-            totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-          />
-        </main>
-      )}
+                )}
+              </div>
+            </section>
+            <Pagination
+              totalPages={totalPages}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
+          </>
+        )}
+      </main>
     </>
   );
 };
