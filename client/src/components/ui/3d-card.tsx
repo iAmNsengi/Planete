2f72353b1be4a4fd9dfc7c37019a96c4b09a5c3a
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-02 07:08:59 +0200

    fix: navbar menu

--- client/src/components/ui/3d-card.tsx
+++ client/src/components/ui/3d-card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { cn } from "../../lib/utils";
@@ -34,12 +35,12 @@ export const CardContainer = ({
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
   };
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseEnter = () => {
     setIsMouseEntered(true);
     if (!containerRef.current) return;
   };
 
-  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseLeave = () => {
     if (!containerRef.current) return;
     setIsMouseEntered(false);
     containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
--- client/src/components/ui/navbar-menu.tsx
+++ client/src/components/ui/navbar-menu.tsx
@@ -2,20 +2,10 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const transition = {
-  type: "spring",
-  mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
-  restDelta: 0.001,
-  restSpeed: 0.001,
-};
 
 export const MenuItem = ({
   setActive,
-  active,
   item,
-  children,
 }: {
   setActive: (item: string) => void;
   active: string | null;
