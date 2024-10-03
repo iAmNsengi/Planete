555958c5d2984a03383e9da80f46ebf328ab14ed
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-03 10:38:38 +0200

    feat: hero highlight

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -12,11 +12,23 @@ const AboutUs = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
       >
-        <h1 className="text-4xl font-bold text-neutral-300 mb-4">About Us</h1>
-        <p className="text-lg text-neutral-400 leading-relaxed">
-          We are a passionate team dedicated to creating elegant solutions. Our
-          mission is to innovate, inspire, and make a positive impact on the
-          world through our work and commitment to excellence.
+        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-700 to-white dark:from-neutral-600 dark:to-white text-5xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          About Us
+          <br />
+        </h2>
+        <p className="text-lg text-neutral-400 leading-relaxed text-left px-4">
+          At Planete Hotel, we believe in creating an experience that goes
+          beyond just a place to stay. Nestled in the heart of the city, our
+          hotel offers a perfect blend of luxury, comfort, and impeccable
+          service. Whether you're visiting for business or leisure, our elegant
+          rooms, world-class amenities, and warm hospitality promise to make
+          your stay truly unforgettable.
+          <br />
+          Our dedicated staff is here to ensure that your every need is met,
+          making you feel at home from the moment you arrive. Discover the best
+          of the city while enjoying the serene ambiance of our hotel. At
+          Planete Hotel, your satisfaction is our priority, and we look forward
+          to welcoming you to a world of sophistication and relaxation.
         </p>
       </motion.div>
     </div>
