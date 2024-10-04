fc10d2edfea85686d3f858935f4993f418febbac
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-04 18:00:37 +0200

    fix: contact us ui

--- client/src/components/ContactUs.tsx
+++ client/src/components/ContactUs.tsx
@@ -13,7 +13,7 @@ const ContactUs = () => {
           </h1>
         </div>
       </div>
-      <div className="max-w-xl w-full mx-auto rounded-none md:rounded-2xl bg-bl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+      <div className="max-w-xl w-full mx-auto rounded-none md:rounded-2xl bg-bl p-4 md:p-8 md:py-32 shadow-input bg-white dark:bg-black">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
           Send us a message
         </h2>
