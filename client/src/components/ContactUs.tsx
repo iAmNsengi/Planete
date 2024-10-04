11686afa3720924200afb8a9c58aea639fc5ba2e
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-04 09:56:19 +0200

    feat: contact section

---
+++ client/src/components/ContactUs.tsx
@@ -0,0 +1,19 @@
+const ContactUs = () => {
+  return (
+    <>
+      <div className="bg-black text-white ">
+        <div className="container mx-auto py-12">
+          <h1 className="text-4xl font-bold text-center">
+            Feel free to contact us! Call{" "}
+            <span className="text-blue-300">+(250) 78-8426737</span>
+          </h1>
+        </div>
+      </div>
+          <form>
+              
+      </form>
+    </>
+  );
+};
+
+export default ContactUs;
