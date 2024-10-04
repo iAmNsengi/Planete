9508edff4743cd7129f5b3299ed4f57e8b45ae8d
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-04 15:12:28 +0200

    feat: login page ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
 import { Login } from "./pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "*",
-    element: <Navigate to="/" />,
+    children: [
+      {
+        index: true,
+        element: <Layout />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -12,7 +12,7 @@ export function Login() {
     alert("Form submitted!");
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md w-full mx-auto rounded-lg absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Login | Planete Hotel Rwanda
       </h2>
@@ -31,7 +31,7 @@ export function Login() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          Login &rarr;
           <BottomGradient />
         </button>
 
