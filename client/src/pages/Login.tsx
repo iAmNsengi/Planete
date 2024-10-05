d321a638e7e3956ec433d85c69beae00bc7e58f6
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-05 17:49:42 +0200

    feat: login ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
-import { IconBrandGoogle, IconX } from "@tabler/icons-react";
+import { IconBrandGoogle, IconLoader, IconX } from "@tabler/icons-react";
 import { cn } from "../lib/utils";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -11,13 +11,16 @@ export function Login() {
   const [formData, setFormData] = useState(INITIAL_FORM_VALUES);
   const [showMessage, setShowMessage] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsSubmitting(true);
     const { username, password } = formData;
     if (username === "" || password === "") {
       setErrorMessage("Please fill in all fields");
       setShowMessage(true);
+      setIsSubmitting(false);
       return;
     }
     try {
@@ -32,6 +35,8 @@ export function Login() {
       setErrorMessage("Invalid username or password");
       setShowMessage(true);
       console.error(error as Error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
   return (
@@ -81,10 +86,17 @@ export function Login() {
         </LabelInputContainer>
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] items-center flex justify-center"
           type="submit"
         >
-          Login &rarr;
+          {isSubmitting ? (
+            <>
+              {" "}
+              <IconLoader /> Logging In...
+            </>
+          ) : (
+            <span>Login &rarr;</span>
+          )}
           <BottomGradient />
         </button>
 
