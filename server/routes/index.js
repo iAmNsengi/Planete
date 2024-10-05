56083c6cb5c4acb7444f2d5b339223d11b41747c
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-05 11:08:05 +0200

    chore: register user route

--- server/routes/index.js
+++ server/routes/index.js
@@ -37,4 +37,42 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // check if user already exists
+    const userExist = await User.findOne({ username });
+    if (userExist)
+      return res.status(400).json({
+        message: "User with given credentials already exists",
+        success: false,
+      });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // create a new user
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    const payload = {
+      user: { id: user.id },
+    };
+
+    await jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+      (err, token) => {
+        if (err) throw new Error(err.message);
+        return res
+          .status(201)
+          .json({ message: "User created successfully", success: true });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message, success: false });
+  }
+});
+
 module.exports = router;
