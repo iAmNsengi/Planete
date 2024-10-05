b08d986187e49db987ca5bb7142fad40232c1da9
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-05 10:53:59 +0200

    chore: login route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+app.use("/api/users", require('./routes/index'))
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to database!");
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch(()=> console.log("Error connecting to database!"));
