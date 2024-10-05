57459f1a2f8b7e75465c4540133c8fff1b74ff09
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-05 12:01:12 +0200

    fix: register route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const allUsers = await User.find()
-        return res.status(200).json({users: allUsers, success:true})
-        
-    } catch (error) {
-        console.error(error.message)
-        return res.status(500).json({message: error.message, success: false})
-    }
-})
-
+  try {
+    const allUsers = await User.find();
+    return res.status(200).json({ users: allUsers, success: true });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message, success: false });
+  }
+});
 
 router.post("/login", async (req, res) => {
   try {
@@ -62,23 +62,24 @@ router.post("/register", async (req, res) => {
       });
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      
     // create a new user
     const user = new User({ username, password: hashedPassword });
-    await user.save();
     const payload = {
       user: { id: user.id },
-    };
+      };
 
     await jwt.sign(
       payload,
       process.env.JWT_SECRET,
       { expiresIn: "1d" },
-      (err, token) => {
+      async (err, token) => {
         if (err) throw new Error(err.message);
+        await user.save();
         return res
           .status(201)
-          .json({ message: "User created successfully", success: true });
+          .json({ message: "User created successfully", token, success: true });
       }
     );
   } catch (error) {
@@ -86,6 +87,5 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: error.message, success: false });
   }
 });
-
 
 module.exports = router;
