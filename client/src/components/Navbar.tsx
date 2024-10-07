958fae2217af3261c3afd066e99ae8e22511ec84
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-07 22:08:02 +0200

    fix: absolute urls scroll to specific section

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = ({ className }: { className?: string }) => {
       }
     }
   }, []);
-  const location = useLocation()
+  const location = useLocation();
 
   useEffect(() => {
     checkAuthentication();
@@ -59,19 +59,19 @@ const Navbar = ({ className }: { className?: string }) => {
         <div className="flex items-center gap-3">
           <MenuItem
             setActive={setActive}
-            href="#about"
+            href="/#about"
             active={active}
             item="About"
           />
           <MenuItem
             setActive={setActive}
-            href="#gallery"
+            href="/#gallery"
             active={active}
             item="Gallery"
           />
           <MenuItem
             setActive={setActive}
-            href="#contact"
+            href="/#contact"
             active={active}
             item="Contact"
           />
