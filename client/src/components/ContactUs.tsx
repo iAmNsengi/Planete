06c44c2d1970ec7d65a1a28a799a9431d283f347
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-14 05:44:23 +0200

    fix: contact us refactor

--- client/src/components/ContactUs.tsx
+++ client/src/components/ContactUs.tsx
@@ -214,10 +214,7 @@ const ContactUs: React.FC = () => {
             ></textarea>
           </LabelInputContainer>
 
-          <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] items-center flex justify-center"
-            type="submit"
-          >
+          <button className=" submitButton" type="submit">
             {isSubmitting ? (
               <>
                 <IconLoader className="mr-2" /> Sending...
