63f742297388e56bf35a494e7881367ad4b25934
iAmNsengi <iamnsengi@icloud.com>
2025-07-28 22:34:22 +0200

    feat: update PageLayout component

--- client/src/components/PageLayout.tsx
+++ client/src/components/PageLayout.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import SocialMediaSidebar from "./SocialMediaSidebar";
 import { PatternOverlay } from "./ui/pattern-overlay";
+import { useLocation } from "react-router-dom";
 
 interface PageLayoutProps {
   children: React.ReactNode;
 }
 
 const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="bg-gradient-to-b from-gray-50 to-white relative min-h-screen">
       <PatternOverlay />
